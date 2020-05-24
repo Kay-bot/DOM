ab505088f704097f00616454782d67675ce0c846
KAY S <KS.KWANRUAN@GMAIL.com>
2020-05-24 17:22:03 +1000

    Ha! didn't know that I can do this for a todo list

--- todo-list/app.js
+++ todo-list/app.js
@@ -1,15 +1,26 @@
-// const button = document.querySelector("button");
-// button.addEventListener("click", () => {
-//   console.log("you clicked me");
+const ul = document.querySelector("ul");
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+  const li = document.createElement("li");
+  li.textContent = "new todo";
+  // ul.append(li)
+  ul.prepend(li);
+});
+
+// const items = document.querySelectorAll("li");
+
+// items.forEach((item) => {
+//   item.addEventListener("click", (e) => {
+//     console.log("event in LI");
+//     e.stopPropagation();
+//     e.target.remove();
+//   });
 // });
 
-const items = document.querySelectorAll("li");
-items.forEach((item) => {
-  item.addEventListener("click", (e) => {
-    // console.log("item clicked");
-    // console.log(e);
-    // console.log(e.target);
-    // console.log(item);
-    e.target.style.textDecoration = "line-through";
-  });
+ul.addEventListener("click", (e) => {
+  // console.log("event in UL");
+  if (e.target.tagName === "LI") {
+    e.target.remove();
+  }
 });
